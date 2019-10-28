148f85c9d836c790cf65aa63779b67c355374729
MOBLY\mateus.ricardo <mateus.ricardo@mobly.com.br>
2019-10-27 23:17:23 -0200

    testando objeto publico

--- s3test.py
+++ s3test.py
@@ -8,14 +8,19 @@ else:
     TMP = 'C:/Users/mateus.ricardo/Desktop/tmp/'
 
 
-# engine = auxtools.MySQLAux('MOVIE').engine()
-# df = pd.DataFrame([{'a':1,'b':2}])
-# print(df)
-# df.to_sql('teste', engine, if_exists='replace', index=False)
+df = pd.DataFrame([{'a':1,'b':2}])
+print(df)
+local_file = TMP+'teste_public.csv'
+remote_file = 'logs/teste_public.csv'
+df.to_csv(local_file,index=False)
+s3 = auxtools.S3Aux('stone-project')
+s3.upload(local_file,remote_file,public=True)
+
+
 
 
 # print(__file__.split('\\')[-1])
 # print(datetime.datetime(2099,1,1,0,0,0))
 
-mail = auxtools.MailAux()
-mail.send_mail('Teste','Teste MailAux','Teste','mateusricardo94@gmail.com','mateus.ricardo@mobly.com.br')
+# mail = auxtools.MailAux()
+# mail.send_mail('Teste','Teste MailAux','Teste','mateusricardo94@gmail.com','mateus.ricardo@mobly.com.br')
