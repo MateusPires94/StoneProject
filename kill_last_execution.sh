7ca7863e8dafe7bb7b37d06d034db72b7dd4e99b
MOBLY\mateus.ricardo <mateus.ricardo@mobly.com.br>
2019-10-29 12:31:02 -0200

    teste

--- kill_last_execution.sh
+++ kill_last_execution.sh
@@ -5,11 +5,3 @@ if [ $result -ge 1 ]
    else
         echo "python is not running"
 fi
-
-result=`ps aux | grep -i "run_dag" | grep -v "grep" | wc -l`
-if [ $result -ge 1 ]
-   then
-        kill -9 $(pgrep -f run_dag)
-   else
-        echo "run_dag is not running"
-fi
